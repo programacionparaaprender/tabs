7592c9901c8d736722aac2676f32aad04826c396
lcorrea <yancel209@gmail.com>
2023-01-13 20:20:00 -0500

    agregando prueba unitaria para ionic angular

--- src/app/tab3/tab3.page.spec.ts
+++ src/app/tab3/tab3.page.spec.ts
@@ -1,26 +1,39 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
-
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
-
+import { TestBed } from '@angular/core/testing';
+import { HttpClientModule, HttpClient } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Injector, NgModule } from "@angular/core";
 import { Tab3Page } from './tab3.page';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { AppComponent } from '../../app/app.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouteReuseStrategy } from '@angular/router';
 
 describe('Tab3Page', () => {
-  let component: Tab3Page;
-  let fixture: ComponentFixture<Tab3Page>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [Tab3Page],
-      imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
+      declarations: [
+        Tab3Page,
+      ],
+      imports: [
+        FormsModule, 
+        ReactiveFormsModule,
+        BrowserModule,
+        IonicModule.forRoot(),
+        HttpClientModule,
+      ],
+      providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
-
-    fixture = TestBed.createComponent(Tab3Page);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('Debe retornar usuario valido', () => {
+    const comp = TestBed.createComponent(Tab3Page);
+    const nombre = comp.componentInstance.registerForm.controls['nombre'];
+    const email = comp.componentInstance.registerForm.controls['email'];
+    const password = comp.componentInstance.registerForm.controls['password'];
+    nombre.setValue('ejemplo123');
+    email.setValue('ejemplo123@prueba.com.pe');
+    password.setValue('123456');
+    expect(comp.componentInstance.registerForm.valid).toEqual(true);
+    });
 });
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { Tio } from '../../app/models/tio';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +10,55 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  login: Observable<Tio[]> | undefined;
+  tio: Tio;
+  nombre = '';
+  email = '';
+  password = '';
+  registerForm: FormGroup;
+	milista = [];
+  constructor(
+    //private tioService: TioService, 
+    private fb: FormBuilder, 
+    public navCtrl: NavController) {
+      this.tio = new Tio("", "", "");
+    this.registerForm = this.fb.group({ 
+        id:0,
+        nombre: ['', Validators.required], 
+        email: ['', Validators.maxLength(32)],
+        password: ['', Validators.required]
+    }); 
+  }
 
-  constructor() {}
 
+  openNavDetailsPage(item:any) {
+    //this.navCtrl.push(NavigationDetailsPage, { item: item });
+    //this.navCtrl.push(MasinfoPage, { item: item });
+  }
+  
+  async onCreate() {
+    if(!this.registerForm.valid){
+      alert('debe llenar el formulario')
+      console.log('debe llenar el formulario')
+      return;
+    }
+    this.tio = new Tio(this.nombre, this.email, this.password);
+    /*
+    var response = await this.tioService.registrar(this.tio);
+    if(response.status==200){
+      const data = response.data;
+      const usuario = data[0];
+      this.store.dispatch(new TaskActions.RegistroUsuario({
+        id: usuario.id,
+        nombre: usuario.nombre,
+        email: usuario.email,
+        password: usuario.password
+      }) )
+      this.router.navigate(['/']);
+      
+    }else{
+      console.log('ocurrio un error')
+    }
+    */
+  }
 }
